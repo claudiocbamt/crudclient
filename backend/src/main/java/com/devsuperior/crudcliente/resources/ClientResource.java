b2403f6ac9254924594a9a7ddee55aed377a4f1d
claudio <claudiocbamt@gmail.com>
2022-09-09 23:18:13 -0300

    update client

--- backend/src/main/java/com/devsuperior/crudcliente/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/crudcliente/resources/ClientResource.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,6 +59,16 @@ public class ClientResource  {
   	
   		return ResponseEntity.created(uri).body(dto);
   	}
+
+
+ @PutMapping(value= "/{id}")
+    public ResponseEntity<ClientDTO> Update(@PathVariable("id")Long Id, @RequestBody ClientDTO dto){
+	    dto = service.update( Id, dto);
+		
+		return ResponseEntity.ok().body(dto);
+	}
+
+
    
 
 }
